e451a6e2ed66728a2a41425c41dda6548da14004
Nazir Ahmad <nazir.ahmad@wundermanthompson.com>
2021-12-28 08:19:54 +0530

    Complete implementation of service and repository

---
+++ CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext _ctx;
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
